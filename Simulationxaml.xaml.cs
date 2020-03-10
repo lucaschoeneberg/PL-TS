35bbc4f655f9c3fc13cde76187d3415cb329a47c
Luca Schöneberg <luca-schoeneberg@outlook.com>
2020-03-10 12:46:23 +0100

    Create Simulationxaml.xaml.cs

---
+++ Simulationxaml.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL_TS
+{
+    /// <summary>
+    /// Interaktionslogik für Simulationxaml.xaml
+    /// </summary>
+    public partial class Simulationxaml : Window
+    {
+        public Simulationxaml()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_check_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
