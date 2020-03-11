9abfec2709349bf6924b1457061b074e7bccb03c
Luca Schöneberg <luca-schoeneberg@outlook.com>
2020-03-11 11:06:06 +0100

    Create edit_maker.xaml.cs

---
+++ edit_maker.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL_TS
+{
+    /// <summary>
+    /// Interaktionslogik für edit_maker.xaml
+    /// </summary>
+    public partial class edit_maker : Window
+    {
+        public edit_maker(string makerID)
+        {
+            InitializeComponent();
+        }
+    }
+}
