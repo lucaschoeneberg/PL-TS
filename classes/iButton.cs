b8ec2917c7cfdfbebc0b536e9d7cb38c48355ebb
Luca Schöneberg <luca-schoeneberg@outlook.com>
2020-03-11 12:02:55 +0100

    Update iButton.cs

--- classes/iButton.cs
+++ classes/iButton.cs
@@ -9,27 +9,22 @@ namespace Serial.IButton
         private SerialPort serport; //SerialPort-Object
 
         //Needed variables
-        private string ids = string.Empty;
-        private string com = "COM6"; //Used COM-Port                               
+        private string ids = string.Empty;                           
         
-        public string read_IDs()
+        public string read_IDs(string Com,int baut)
         {
             //Only for test if the data reading works fine
             try
             {
-                serialinit();
+                serialinit(Com, baut);
                 serport.Open(); //Opens the serial-connection
 
                 ids = serport.ReadLine(); //Reads the data from the serial connection                
-                
+                serport.Close(); //Closing the serial-connection
                 //if (ids.Length == 20) //Check if the data stored in IDs is a valid
                 return ids; //Output of the valid IDs
                 //else
                     //MessageBox.Show("IDs fehlerhaft!"); //Error if the data in "mac" is not a valid MAC-Address
-
-                ids = "";
-
-                serport.Close(); //Closing the serial-connection
             }
 
             catch (Exception ex)
@@ -42,10 +37,10 @@ namespace Serial.IButton
             return "-";
         }
         
-        private void serialinit()
+        private void serialinit(string Com, int baut)
         {
-            serport = new SerialPort(com); //New instance of the SerialPort-Class with the used COM-Interface as transfer parameter
-            serport.BaudRate = 115200; //Setting the BaudRate to 9600
+            serport = new SerialPort(Com); //New instance of the SerialPort-Class with the used COM-Interface as transfer parameter
+            serport.BaudRate = baut; //Setting the BaudRate to 9600
             serport.Parity = Parity.None; //Setting Parity to None
             serport.DataBits = 8; //Used Bits for data is set to 8
             serport.StopBits = StopBits.One; //StopBit is set to 1
