849238595b5d3be6dd38b642d9e22c3d53cacb8c
Luca Schöneberg <37987646+lucaschoeneberg@users.noreply.github.com>
2020-03-12 17:06:53 +0100

    Update PL_Login.xaml.cs
    add if keymember

--- PL_Login.xaml.cs
+++ PL_Login.xaml.cs
@@ -20,18 +20,19 @@ namespace PL_TS
             List<string[]> Logindaten = new List<string[]>();
             Logindaten = data.CommandSelectAsListFrom("user", " WHERE E_Mail='" + tbx_username.Text + "' OR Benutzername='" + tbx_username.Text + "'");
             if (Logindaten.Count != 0)
-            {
-                if ((tbx_username.Text == Logindaten[0][3] || tbx_username.Text == Logindaten[0][5]) && BCrypt.Net.BCrypt.Verify(tbx_password.Password, Logindaten[0][6]))
-                {
-                    Main openMain = new Main();
-                    this.Visibility = Visibility.Hidden;
-                    openMain.ShowDialog();
-                }
-            }
+                if (Logindaten[0][4] == "True")
+                    if ((tbx_username.Text == Logindaten[0][3] || tbx_username.Text == Logindaten[0][5]) && BCrypt.Net.BCrypt.Verify(tbx_password.Password, Logindaten[0][6]))
+                    {
+                        Main openMain = new Main();
+                        this.Visibility = Visibility.Hidden;
+                        openMain.ShowDialog();
+                    }
+                    else
+                        MessageBox.Show("Password oder Benutzername Falsch!");
+                else
+                    MessageBox.Show("Kein KeyMember mehr!");
             else
-            {
-                MessageBox.Show("Ihre Eingaben sind falsch!");
-            }
+                MessageBox.Show("Password und Benutzername eingeben!");
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
