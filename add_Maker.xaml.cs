b50ff0d34b4addf6aec2b4702f3080e23b696817
Ofenhammer-Lars <60092820+Ofenhammer-Lars@users.noreply.github.com>
2020-03-13 14:56:04 +0100

    Update add_Maker.xaml.cs

--- add_Maker.xaml.cs
+++ add_Maker.xaml.cs
@@ -29,7 +29,7 @@ namespace PL_TS
 
         private void Chb_Keymember_Checked(object sender, RoutedEventArgs e)
         {
-            if (chb_Keymember.IsChecked == true)
+            if (chb_Keymember.IsChecked == true) //Nur für Keymembereingaben nötig
             {
                 tbx_Benutzername.Visibility = Visibility.Visible;
                 tbx_password.Visibility = Visibility.Visible;
@@ -41,7 +41,7 @@ namespace PL_TS
         }
         private void Chb_Keymember_Unchecked(object sender, RoutedEventArgs e)
         {
-            if (chb_Keymember.IsChecked == false)
+            if (chb_Keymember.IsChecked == false) //Einige Felder sind für den Nutzer nicht Sichtbar, weil sie keine Keymemberinfos benötigen
             {
                 tbx_Benutzername.Visibility = Visibility.Hidden;
                 tbx_password.Visibility = Visibility.Hidden;
@@ -89,23 +89,23 @@ namespace PL_TS
 
         private void Btn_addMaker_Click(object sender, RoutedEventArgs e)
         {
-            if (tbx_Vorname.Text != "" || tbx_Nachname.Text != "" || tbx_EMail.Text != "")
+            if (tbx_Vorname.Text != "" || tbx_Nachname.Text != "" || tbx_EMail.Text != "") //Wird nur ausgeführt, wenn die Felder gefüllt sind.
             {
                 IButton readButton = new IButton();
                 Dbase data = new Dbase("localhost", "projektlabor", "root", "");
                 string ID = readButton.read_IDs(cbx_com.SelectedItem.ToString(), 115000);
                 lbl_iButton.Content = ID.Split(';')[0];
-                data.CommandInsertInto("ibutton", "iButtonID, Typ", "'" + ID.Split(';')[0] + "','DS1990A'");
+                data.CommandInsertInto("ibutton", "iButtonID, Typ", "'" + ID.Split(';')[0] + "','DS1990A'");//Einfügen eines nicht vorhandenen iButtons
                 if (chb_Keymember.IsChecked == true)
                 {
                     if (tbx_Benutzername.Text != "" || tbx_password.Password != "" || tbx_password_verify.Password != "" && tbx_password.Password == tbx_password_verify.Password)
                     {
-                        data.CommandInsertInto("user", "Vorname, Nachname, E_Mail, Keymember, Benutzername, Passwort, iButtonID", "'" + tbx_Vorname.Text + "','" + tbx_Nachname.Text + "','" + tbx_EMail.Text + "'," + 1 + ",'" + tbx_Benutzername.Text + "','" + tbx_password.Password + "','" + ID.Split(';')[0] + "'");
+                        data.CommandInsertInto("user", "Vorname, Nachname, E_Mail, Keymember, Benutzername, Passwort, iButtonID", "'" + tbx_Vorname.Text + "','" + tbx_Nachname.Text + "','" + tbx_EMail.Text + "'," + 1 + ",'" + tbx_Benutzername.Text + "','" + tbx_password.Password + "','" + ID.Split(';')[0] + "'"); //Einfügen eines Keymembers
                     }
                 }
                 else
                 {
-                    data.CommandInsertInto("user", "Vorname, Nachname, E_Mail, Keymember, iButtonID", "'" + tbx_Vorname.Text + "','" + tbx_Nachname.Text + "','" + tbx_EMail.Text + "'," + 0 + ",'" + ID.Split(';')[0] + "'");
+                    data.CommandInsertInto("user", "Vorname, Nachname, E_Mail, Keymember, iButtonID", "'" + tbx_Vorname.Text + "','" + tbx_Nachname.Text + "','" + tbx_EMail.Text + "'," + 0 + ",'" + ID.Split(';')[0] + "'"); //Einfügen eines normalen Makers
 
                 }
             }
