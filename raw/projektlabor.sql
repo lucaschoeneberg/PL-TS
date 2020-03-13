cb10e4c69040ca9cad085da7d21607f3ace1d939
Luca Schöneberg <37987646+lucaschoeneberg@users.noreply.github.com>
2020-03-13 17:47:30 +0100

    Update projektlabor.sql

--- raw/projektlabor.sql
+++ raw/projektlabor.sql
@@ -1,230 +1,168 @@
--- phpMyAdmin SQL Dump
--- version 4.9.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Erstellungszeit: 06. Mrz 2020 um 19:46
--- Server-Version: 10.4.8-MariaDB
--- PHP-Version: 7.3.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `projektlabor` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci */;
-
+CREATE DATABASE  IF NOT EXISTS `projektlabor` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci */;
 USE `projektlabor`;
+-- MariaDB dump 10.17  Distrib 10.4.10-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: projektlabor
+-- ------------------------------------------------------
+-- Server version	10.4.10-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Datenbank: `projektlabor`
+-- Table structure for table `ibutton`
 --
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `ibutton`
---
-
+DROP TABLE IF EXISTS `ibutton`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ibutton` (
   `iButtonID` varchar(16) COLLATE utf8mb4_german2_ci NOT NULL,
-  `Typ` varchar(15) COLLATE utf8mb4_german2_ci NOT NULL
+  `Typ` varchar(15) COLLATE utf8mb4_german2_ci NOT NULL,
+  UNIQUE KEY `iButtonID` (`iButtonID`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `ibutton`
+-- Dumping data for table `ibutton`
 --
 
-INSERT INTO `ibutton` (`iButtonID`, `Typ`) VALUES
-('48EAE319008BA100', 'DS1990A'),
-('9E31E31900D3A100', 'DS1990A'),
-('A225E41900A6A100', 'DS1990A'),
-('A4D2E21900EAA100', 'DS1990A'),
-('C08B241700574100', 'DS1990A'),
-('EE2ADB190013A100', 'DS1990A'),
-('FF30E419006C4100', 'DS1990A');
-
--- --------------------------------------------------------
+LOCK TABLES `ibutton` WRITE;
+/*!40000 ALTER TABLE `ibutton` DISABLE KEYS */;
+INSERT INTO `ibutton` VALUES ('48EAE319008BA100','DS1990A'),('9E31E31900D3A100','DS1990A'),('A225E41900A6A100','DS1990A'),('A4D2E21900EAA100','DS1990A'),('A570017248BC0','DS1990A'),('ACA001B9E2B0','DS1990A'),('C08B241700574100','DS1990A'),('EE2ADB190013A100','DS1990A'),('FF30E419006C4100','DS1990A');
+/*!40000 ALTER TABLE `ibutton` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Tabellenstruktur für Tabelle `log`
+-- Table structure for table `log`
 --
 
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `log` (
-  `LogID` int(10) NOT NULL,
+  `LogID` int(10) NOT NULL AUTO_INCREMENT,
   `iButtonID` varchar(16) COLLATE utf8mb4_german2_ci NOT NULL,
   `MaschinenID` varchar(20) COLLATE utf8mb4_german2_ci NOT NULL,
   `Starttime` datetime NOT NULL,
-  `Endtime` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
+  `Endtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`LogID`),
+  KEY `iButtonID` (`iButtonID`),
+  KEY `MaschinenID` (`MaschinenID`),
+  CONSTRAINT `log_ibfk_3` FOREIGN KEY (`iButtonID`) REFERENCES `ibutton` (`iButtonID`),
+  CONSTRAINT `log_ibfk_4` FOREIGN KEY (`MaschinenID`) REFERENCES `maschine` (`MaschinenID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `log`
+-- Dumping data for table `log`
 --
 
-INSERT INTO `log` (`LogID`, `iButtonID`, `MaschinenID`, `Starttime`, `Endtime`) VALUES
-(1, 'A4D2E21900EAA100', 'pl3drb', '2020-02-21 08:15:12', '2020-02-21 10:23:01'),
-(2, 'FF30E419006C4100', 'pl4sbs', '2020-02-27 10:24:24', '2020-02-27 10:53:41');
-
--- --------------------------------------------------------
+LOCK TABLES `log` WRITE;
+/*!40000 ALTER TABLE `log` DISABLE KEYS */;
+INSERT INTO `log` VALUES (1,'A4D2E21900EAA100','pl3drb','2020-02-21 08:15:12','2020-02-21 10:23:01'),(2,'FF30E419006C4100','pl4sbs','2020-02-27 10:24:24','2020-02-27 10:53:41');
+/*!40000 ALTER TABLE `log` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Tabellenstruktur für Tabelle `maschine`
+-- Table structure for table `maschine`
 --
 
+DROP TABLE IF EXISTS `maschine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `maschine` (
   `MaschinenID` varchar(20) COLLATE utf8mb4_german2_ci NOT NULL,
-  `Bezeichnung` varchar(30) COLLATE utf8mb4_german2_ci NOT NULL
+  `Bezeichnung` varchar(30) COLLATE utf8mb4_german2_ci NOT NULL,
+  PRIMARY KEY (`MaschinenID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `maschine`
+-- Dumping data for table `maschine`
 --
 
-INSERT INTO `maschine` (`MaschinenID`, `Bezeichnung`) VALUES
-('pl1cnc', 'CNC-Maschine'),
-('pl23dd', '3D-Drucker'),
-('pl3drb', 'Drehbank'),
-('pl4sbs', 'Schubladensystem');
-
--- --------------------------------------------------------
+LOCK TABLES `maschine` WRITE;
+/*!40000 ALTER TABLE `maschine` DISABLE KEYS */;
+INSERT INTO `maschine` VALUES ('pl1cnc','CNC-Maschine'),('pl23dd','3D-Drucker'),('pl3drb','Drehbank'),('pl4sbs','Schubladensystem');
+/*!40000 ALTER TABLE `maschine` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Tabellenstruktur für Tabelle `user`
+-- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `UserID` int(10) NOT NULL,
+  `UserID` int(10) NOT NULL AUTO_INCREMENT,
   `Vorname` varchar(20) COLLATE utf8mb4_german2_ci NOT NULL,
   `Nachname` varchar(30) COLLATE utf8mb4_german2_ci NOT NULL,
   `E_Mail` varchar(40) COLLATE utf8mb4_german2_ci NOT NULL,
   `Keymember` tinyint(1) NOT NULL,
   `Benutzername` varchar(20) COLLATE utf8mb4_german2_ci DEFAULT NULL,
-  `Passwort` varchar(30) COLLATE utf8mb4_german2_ci DEFAULT NULL,
-  `iButtonID` varchar(16) COLLATE utf8mb4_german2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
+  `Passwort` varchar(100) COLLATE utf8mb4_german2_ci DEFAULT NULL,
+  `iButtonID` varchar(16) COLLATE utf8mb4_german2_ci NOT NULL,
+  PRIMARY KEY (`UserID`),
+  KEY `iButtonID` (`iButtonID`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`iButtonID`) REFERENCES `ibutton` (`iButtonID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`UserID`, `Vorname`, `Nachname`, `E_Mail`, `Keymember`, `Benutzername`, `Passwort`, `iButtonID`) VALUES
-(1, 'Berthold', 'Sommer', 'b.sommer@berufskolleg-rheine.de', 1, 'somm', 'pass123', 'FF30E419006C4100'),
-(2, 'Damian', 'Zdanowitsch', 'd.zdanowicz@berufskolleg-rheine.de', 1, 'zdan', 'pass123', 'C08B241700574100'),
-(3, 'Luca', 'Schöneberg', 'ls@gmx.de', 0, NULL, NULL, 'A4D2E21900EAA100'),
-(4, 'Maximilian', 'Musterknabe', 'mm@muster.de', 0, NULL, NULL, 'A225E41900A6A100');
-
--- --------------------------------------------------------
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Berthold','Sommer','b.sommer@berufskolleg-rheine.de',1,'somm','$2a$11$BaquQT.bkS1hR/8XLUQg9umoioV3BGutiiy10Kqb3752zTaSyaR9C','FF30E419006C4100'),(2,'Damian','Zdanowitsch','d.zdanowicz@berufskolleg-rheine.de',1,'zdan','pass123','C08B241700574100'),(3,'Luca','Schöneberg','ls@gmx.de',0,'schoeneberg','$2a$11$9s/r98RrGbk9m9DE2uNWzOijZNjqfjdmFKhj4AUVU0N0D7yLrb5KK','A4D2E21900EAA100');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Tabellenstruktur für Tabelle `zuweisung`
+-- Table structure for table `zuweisung`
 --
 
+DROP TABLE IF EXISTS `zuweisung`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `zuweisung` (
   `iButtonID` varchar(16) COLLATE utf8mb4_german2_ci NOT NULL,
   `MaschinenID` varchar(20) COLLATE utf8mb4_german2_ci NOT NULL,
-  `Datum` date NOT NULL
+  `Datum` date NOT NULL,
+  PRIMARY KEY (`MaschinenID`,`iButtonID`),
+  KEY `iButtonID` (`iButtonID`),
+  CONSTRAINT `zuweisung_ibfk_3` FOREIGN KEY (`iButtonID`) REFERENCES `ibutton` (`iButtonID`),
+  CONSTRAINT `zuweisung_ibfk_4` FOREIGN KEY (`MaschinenID`) REFERENCES `maschine` (`MaschinenID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `zuweisung`
+-- Dumping data for table `zuweisung`
 --
 
-INSERT INTO `zuweisung` (`iButtonID`, `MaschinenID`, `Datum`) VALUES
-('A4D2E21900EAA100', 'pl1cnc', '2020-03-05'),
-('FF30E419006C4100', 'pl1cnc', '2020-03-04'),
-('A4D2E21900EAA100', 'pl23dd', '2020-03-04'),
-('C08B241700574100', 'pl23dd', '2020-03-05'),
-('FF30E419006C4100', 'pl23dd', '2020-03-04'),
-('A225E41900A6A100', 'pl3drb', '2020-03-05'),
-('A4D2E21900EAA100', 'pl3drb', '2020-03-05'),
-('FF30E419006C4100', 'pl3drb', '2020-03-04'),
-('FF30E419006C4100', 'pl4sbs', '2020-03-04');
+LOCK TABLES `zuweisung` WRITE;
+/*!40000 ALTER TABLE `zuweisung` DISABLE KEYS */;
+INSERT INTO `zuweisung` VALUES ('ACA001B9E2B0','pl1cnc','2020-03-13'),('FF30E419006C4100','pl1cnc','2020-03-04'),('ACA001B9E2B0','pl23dd','2020-03-13'),('C08B241700574100','pl23dd','2020-03-05'),('FF30E419006C4100','pl23dd','2020-03-04'),('A225E41900A6A100','pl3drb','2020-03-05'),('ACA001B9E2B0','pl3drb','2020-03-13'),('FF30E419006C4100','pl3drb','2020-03-04'),('ACA001B9E2B0','pl4sbs','2020-03-13'),('FF30E419006C4100','pl4sbs','2020-03-04');
+/*!40000 ALTER TABLE `zuweisung` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indizes der exportierten Tabellen
---
-
---
--- Indizes für die Tabelle `ibutton`
---
-ALTER TABLE `ibutton`
-  ADD UNIQUE KEY `iButtonID` (`iButtonID`) USING BTREE;
-
---
--- Indizes für die Tabelle `log`
---
-ALTER TABLE `log`
-  ADD PRIMARY KEY (`LogID`),
-  ADD KEY `iButtonID` (`iButtonID`),
-  ADD KEY `MaschinenID` (`MaschinenID`);
-
---
--- Indizes für die Tabelle `maschine`
---
-ALTER TABLE `maschine`
-  ADD PRIMARY KEY (`MaschinenID`);
-
---
--- Indizes für die Tabelle `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`UserID`),
-  ADD KEY `iButtonID` (`iButtonID`);
-
---
--- Indizes für die Tabelle `zuweisung`
---
-ALTER TABLE `zuweisung`
-  ADD PRIMARY KEY (`MaschinenID`,`iButtonID`),
-  ADD KEY `iButtonID` (`iButtonID`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `log`
---
-ALTER TABLE `log`
-  MODIFY `LogID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT für Tabelle `user`
---
-ALTER TABLE `user`
-  MODIFY `UserID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `log`
---
-ALTER TABLE `log`
-  ADD CONSTRAINT `log_ibfk_3` FOREIGN KEY (`iButtonID`) REFERENCES `ibutton` (`iButtonID`),
-  ADD CONSTRAINT `log_ibfk_4` FOREIGN KEY (`MaschinenID`) REFERENCES `maschine` (`MaschinenID`);
-
---
--- Constraints der Tabelle `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`iButtonID`) REFERENCES `ibutton` (`iButtonID`);
-
---
--- Constraints der Tabelle `zuweisung`
---
-ALTER TABLE `zuweisung`
-  ADD CONSTRAINT `zuweisung_ibfk_3` FOREIGN KEY (`iButtonID`) REFERENCES `ibutton` (`iButtonID`),
-  ADD CONSTRAINT `zuweisung_ibfk_4` FOREIGN KEY (`MaschinenID`) REFERENCES `maschine` (`MaschinenID`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-13 17:46:07
